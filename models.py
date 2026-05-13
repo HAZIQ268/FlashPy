16534e9bb2213f3fb9fc7f4a3e32de2397ceb5a4
Haziq <153829508+HAZIQ268@users.noreply.github.com>
2026-05-13 20:38:15 +0500

    Integrate MongoDB for user authentication
    Refactor User class to use MongoDB for user data storage and retrieval.

--- models.py
+++ models.py
@@ -1,9 +1,40 @@
-from flask_login import UserMixin
+# models.py
 
-# Fake DB
-users_db = {}
-#User class for authentication
+from flask_login import UserMixin
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+# MongoDB connection
+client = MongoClient("mongodb+srv://samrasadaqat:admin12345@backend1.k7uropq.mongodb.net/?appName=backend1")
+db = client["flask_auth_db"]
+users_collection = db["users"]
+
 class User(UserMixin):
-    def __init__(self, username, password):
-        self.id = username
-        self.password = password
+    def __init__(self, user_data):
+        self.id = str(user_data.get("_id"))
+        self.username = user_data.get("username")
+        self.password = user_data.get("password")  # hashed password
+
+    @staticmethod
+    def get_by_username(username):
+        user_data = users_collection.find_one({"username": username})
+        return User(user_data) if user_data else None
+
+    @staticmethod
+    def get_by_id(user_id):
+        try:
+            user_data = users_collection.find_one({"_id": ObjectId(user_id)})
+            return User(user_data) if user_data else None
+        except:
+            return None
+
+    @staticmethod
+    def create(username, hashed_password):
+        # Check if user already exists
+        if users_collection.find_one({"username": username}):
+            return None
+        user_id = users_collection.insert_one({
+            "username": username,
+            "password": hashed_password
+        }).inserted_id
+        return User.get_by_id(user_id)
